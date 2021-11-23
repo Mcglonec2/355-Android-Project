7570d6c9917d3d3fe2d75706d17eb1575a0c1286
mcglonec2 <mcglonec2@vcu.edu>
2021-11-23 03:51:49 -0500

    Connecting Play button

---
+++ app/src/main/java/com/example/fimas/DifficultyScreen.java
@@ -0,0 +1,14 @@
+package com.example.fimas;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DifficultyScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.difficulty_screen);
+    }
+}
--- app/src/main/java/com/example/fimas/MainActivity.java
+++ app/src/main/java/com/example/fimas/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.fimas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting");
+
+        Button play = (Button) findViewById(R.id.playButton);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked play button");
+
+                Intent intent = new Intent(MainActivity.this, DifficultyScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
