db425ecfa5f1845e72497578f92930b6149c7437
mcglonec2 <mcglonec2@vcu.edu>
2021-12-13 05:02:41 -0500

    Added buttons / Remade screens

--- app/src/main/java/com/example/fimas/MainActivity.java
+++ app/src/main/java/com/example/fimas/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Starting");
 
-        /* Button play = (Button) findViewById(R.id.playButton);
+        Button play = (Button) findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,15 +34,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button scores = (Button) findViewById(R.id.scoreButton);
+        Button scores = (Button) findViewById(R.id.scores);
         scores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: Clicked play button");
+                Log.d(TAG, "onClick: Clicked scores button");
 
                 Intent intent = new Intent(MainActivity.this, ScoresScreen.class);
                 startActivity(intent);
             }
-        }); */
+        });
+
+        Button background = (Button) findViewById(R.id.BGChange);
+        RelativeLayout layout = findViewById(R.id.StartScreen);
+        scores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked backround change button");
+
+
+            }
+        });
     }
 }
